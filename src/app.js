17fa3d180134c63e334edf97eef5dc9ca5962b00
Jack Tran <jacktran@Jacks-MBP.attlocal.net>
2017-05-27 13:59:23 -0500

    First time

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import { Footer } from './components'
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Jack Tran</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
